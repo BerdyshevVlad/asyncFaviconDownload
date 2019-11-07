d0badfe0a2b6bef11826f2fc0c2693ff25486ec1
Vladyslavb <Vladyslavb@SW-DEV310>
2019-11-07 16:44:29 +0200

    init whenAll

--- FaviconBrowser/MainWindow.xaml.cs
+++ FaviconBrowser/MainWindow.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Net;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media.Imaging;
@@ -15,80 +17,87 @@ namespace FaviconBrowser
 
     // book: Async https://777russia.ru/book/uploads/%D0%9F%D0%A0%D0%9E%D0%93%D0%A0%D0%90%D0%9C%D0%9C%D0%98%D0%A0%D0%9E%D0%92%D0%90%D0%9D%D0%98%D0%95/C%23/Async%20in%20C%23%205.0%2C%202012%20%28%D0%BF%D0%B5%D1%80%D0%B5%D0%B2%D0%BE%D0%B4%2C%20%D0%BD%D0%B0%20%D1%80%D1%83%D1%81%D1%81%D0%BA%D0%BE%D0%BC%29.pdf
     // Rihter: https://viduus.net/wp-content/uploads/2018/02/Rihter-Dzh.-CLR-via-C.-Programmirovanie-na-platforme-Microsoft-.NET-Framework-4.5-na-yazyke-C-Master-klass-2013.pdf
+
+
+    public class Domains
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+
     public partial class MainWindow : Window
     {
         private static readonly List<string> s_Domains = new List<string>
                                                              {
                                                                  "google.com",
                                                                  "bing.com",
+                                                                 "oreilly.com",
+                                                                 "simple-talk.com",
+                                                                 "microsoft.com",
                                                                  "facebook.com",
+                                                                 "twitter.com",
                                                                  "reddit.com",
                                                                  "baidu.com",
                                                                  "bbc.co.uk"
                                                              };
+
+        private readonly List<Domains> DomainsClass = new List<Domains>()
+        {
+            new Domains()
+            {
+                Id=1,
+                Name = "google.com"
+            },
+            new Domains()
+            {
+                Id=2,
+                Name =  "bing.com",
+            },
+            new Domains()
+            {
+                Id=3,
+                Name = "oreilly.com",
+            },
+            new Domains()
+            {
+                Id=4,
+                Name = "facebook.com",
+            },
+        };
 
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        private void GetButton_OnClick(object sender, RoutedEventArgs e)
+        private async void GetButton_OnClick(object sender, RoutedEventArgs e)
         {
-            foreach (string domain in s_Domains)
+            IEnumerable<Task<Image>> tasks = s_Domains.Select(GetFavicon);
+            //or
+            //IEnumerable<Task<Image>> tasks = s_Domains.Select(x=>GetFavicon(x));
+            //IEnumerable<Task<Image>> tasks2 = DomainsClass.Select(x=>GetFavicon(x.Name));
+
+
+            Task<Image[]> allTask = Task.WhenAll(tasks);
+            Image[] images = await allTask;
+            foreach (Image eachImage in images)
             {
-                AddAFavicon(domain);
+                AddAFavicon(eachImage);
             }
         }
 
-        ////ordered
-        //private void GetButton_OnClick(object sender, RoutedEventArgs e)
-        //{
-        //    AddRemainingFavicons(s_Domains, 0);
-        //}
-
-        ////ordered
-        //private void AddRemainingFavicons(List<string> domains, int i)
-        //{
-        //    WebClient webClient = new WebClient();
-        //    webClient.DownloadDataCompleted += (o, args) =>
-        //    {
-        //        Image imageControl = MakeImageControl(args.Result);
-        //        m_WrapPanel.Children.Add(imageControl);
-
-        //        if (i + 1 < domains.Count)
-        //        {
-        //            AddRemainingFavicons(domains, i + 1);
-        //        }
-        //    };
-        //    webClient.DownloadDataAsync(new Uri("http://" + domains[i] + "/favicon.ico"));
-        //}
-
-
-        //один поток для выполнения кода, второй поток - очередь выполнения. Идет выполнение в одном потоке, когда код проходит строку 73- m_WrapPanel.Children.Add(imageControl); в очередь выполнения добавляется изменения по отрисовке компонентов. Но т.к. это синхронное выполнение, то необходимо дождаться завершения кода (загрузки всех картинок), а потом отработает отрисовка(из очереди выполнения).
-        private void AddAFavicon(string domain)
+        private async Task<Image> GetFavicon(string domain)
         {
             WebClient webClient = new WebClient();
-            byte[] bytes = webClient.DownloadData("http://" + domain + "/favicon.ico");
-            Image imageControl = MakeImageControl(bytes);
-            m_WrapPanel.Children.Add(imageControl);
+            byte[] bytes = await webClient.DownloadDataTaskAsync("http://" + domain + "/favicon.ico");
+            return MakeImageControl(bytes);
         }
 
-
-        ////async 
-        //private void AddAFavicon(string domain)
-        //{
-        //    ServicePointManager.SecurityProtocol = SecurityProtocolType.Ssl3 | SecurityProtocolType.Tls;
-        //    WebClient webClient = new WebClient();
-        //    webClient.DownloadDataCompleted += OnWebClientOnDownloadDataCompleted;
-        //    webClient.DownloadDataAsync(new Uri("http://" + domain + "/favicon.ico"));
-        //}
-
-//        private void OnWebClientOnDownloadDataCompleted(object sender,
-//DownloadDataCompletedEventArgs args)
-//        {
-//            Image imageControl = MakeImageControl(args.Result);
-//            m_WrapPanel.Children.Add(imageControl);
-//        }
+        private void AddAFavicon(Image imageControl)
+        {
+            m_WrapPanel.Children.Add(imageControl);
+        }
 
         private static Image MakeImageControl(byte[] bytes)
         {
@@ -104,3 +113,4 @@ namespace FaviconBrowser
         }
     }
 }
+
